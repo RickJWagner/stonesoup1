d193fdd2828da665d23a91e3d3c72a96398b4735
Rick <rick@rwagner.attlocal.net>
2023-03-07 14:47:58 -0600

    copy from Burrs java-springboot-hello

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Date;
+
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+    int cnt = 1;
+    private static final String HOSTNAME = 
+        System.getenv().getOrDefault("HOSTNAME", "unknown");
+
+    private static final String GREETING =
+        System.getenv().getOrDefault("GREETING", "10 Hola");
+
+    
+    @RequestMapping("/")
+    String home() {
+      Locale locale;
+      DateFormat dateFormat; 
+      String pattern; 
+      SimpleDateFormat simpleDateFormat; 
+    
+      locale = new Locale("en", "US");
+      dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+      // pattern = "dd-M-yyyy hh:mm:ss"; // euro
+      pattern = "M-dd-yyyy hh:mm:ss"; // us
+      simpleDateFormat = new SimpleDateFormat(pattern);
+      String stringNow = simpleDateFormat.format(new Date());
+
+      System.out.println(GREETING + ": " + cnt);
+      return GREETING + ": " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
